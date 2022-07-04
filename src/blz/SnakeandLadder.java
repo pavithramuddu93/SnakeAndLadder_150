a27a7f42d9ebe88e08b6cecfd24e66eb87ba61d2
pavithramuddu93 <pavithramuddu93>
2022-07-04 21:09:42 +0530

    player

--- src/blz/SnakeandLadder.java
+++ src/blz/SnakeandLadder.java
@@ -1,10 +1,30 @@
 package blz;
 
 public class SnakeandLadder {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		int player1Position=0;
+		Random random=new Random();
+		int Dice=1+random.nextInt(6);
+		System.out.println("dice value: "+Dice);
+		Random random1=new Random();
+		int playOption=random1.nextInt(3);
+		System.out.println("play option: "+playOption);
+		switch (playOption) {
+		case 0:
+			System.out.println(" NO PLAY "+" Player position is " +player1Position);
+			break;
+		case 1:
+			player1Position=player1Position+Dice;
+			System.out.println(" Ladder!! \n Player position is " +player1Position);
+			break;
 
+		default:
+			player1Position=player1Position-Dice;
+			System.out.println(" Snake "+" Player position is " +player1Position);
+			break;
+		}
 	}
 
 }
